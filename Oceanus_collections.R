ccda7cf3673413c07fe721dc90dafb6d3de4d5c8
Ross Whippo <rosspitality@gmail.com>
2019-09-11 11:00:29 -0700

    Added collection analysis script

---
+++ Oceanus_collections.R
@@ -0,0 +1,77 @@
+###################################################################################
+#                                                                                ##
+# Oceanus Sample Collections                                                     ##
+# Data are current as of 2019-09-11                                              ##
+# Data source: OIMB                                                              ##
+# R code prepared by Ross Whippo                                                 ##
+# Last updated 2019-09-11                                                        ##
+#                                                                                ##
+###################################################################################
+
+# SUMMARY:
+# Data from Oceanus 2019 organism collections including summaries of taxa and basic
+# QAQC of the dataset.
+
+# Required Files (check that script is loading latest version):
+# Oceanus_FA-collections_2019.csv
+# Oceanus_Collection_Sites.csv
+
+# Associated Scripts:
+# none
+
+# TO DO
+
+###################################################################################
+# TABLE OF CONTENTS                                                               #
+#                                                                                 #
+# RECENT CHANGES TO SCRIPT                                                        #
+# LOAD PACKAGES                                                                   #
+# READ IN AND PREPARE DATA                                                        #
+# MANIPULATE DATA                                                                 #   
+#                                                                                 #
+###################################################################################
+
+###################################################################################
+# RECENT CHANGES TO SCRIPT                                                        #
+###################################################################################
+
+# 2019-09-11 Script created
+
+###################################################################################
+# LOAD PACKAGES                                                                   #
+###################################################################################
+
+library(tidyverse)
+
+###################################################################################
+# READ IN AND PREPARE DATA                                                        #
+###################################################################################
+
+Oceanus_taxa <- read_csv("Oceanus_FA-collections_2019.csv")
+str(Oceanus_taxa)
+
+# fix incorrect site data
+Oceanus_taxa$SITE <- Oceanus_taxa$SITE %>%
+  recode("LBV04" = "NPV04")
+
+Oceanus_sites <- read_csv("Oceanus_Collection_Sites.csv")
+str(Oceanus_sites)
+
+Oceanus_full <- left_join(Oceanus_taxa, Oceanus_sites, by = 'SITE')
+Oceanus_full <- Oceanus_full %>%
+  mutate(MEAN_DEPTH = rowMeans(select(Oceanus_full, MAX_DEPTH, MIN_DEPTH)))
+
+###################################################################################
+# MANIPULATE DATA                                                                 #
+###################################################################################
+
+# list of taxa collected by depth
+taxa_summary <- Oceanus_full %>%
+  select(ORGANISM, MEAN_DEPTH) %>%
+  group_by(ORGANISM, MEAN_DEPTH) %>%
+  distinct()
+
+############### SUBSECTION HERE
+
+#####
+#<<<<<<<<<<<<<<<<<<<<<<<<<<END OF SCRIPT>>>>>>>>>>>>>>>>>>>>>>>>#
